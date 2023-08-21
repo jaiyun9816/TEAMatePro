1c32b587ed2d791fd67ee1d9a767548f3ff97a10
Jaiyun Lee <jaiyun16@gmail.com>
2023-08-21 17:45:56 +0900

    update

--- analysis/main.py
+++ analysis/main.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
         {"name" : "sy", "time" : datetime.strptime("2023-08-07 17:47:02", "%Y-%m-%d %H:%M:%S"), "text" : "안녕하세요"},
         {"name" : "sy", "time" : datetime.strptime("2023-08-08 17:46:02", "%Y-%m-%d %H:%M:%S"), "text" : "집가고싶다"},
     ]
-    para = {"type" : "short", "timeblock" : 10, "file" : 1, "photo" : 1, "pingpong" : {5 : 1, 7 : 1.5, 10 : 3, 20 : 5, "full" : 8}, "nonpingpong" : {5 : 0, 7 : 0, 10 : 1, 20 : 2, "full" : 3}, "starter" : 1}
+    para = {"type" : "short", "timeblock" : 600, "file" : 1, "photo" : 1, "pingpong" : {5 : 1, 7 : 1.5, 10 : 3, 20 : 5, "full" : 8}, "nonpingpong" : {5 : 0, 7 : 0, 10 : 1, 20 : 2, "full" : 3}, "starter" : 1}
     ##timeblock 기준 : min
     
     df = pd.DataFrame(text)
@@ -34,5 +34,6 @@ if __name__ == "__main__":
     engine.set_env_source(context.get_source())
     
     engine.run_multi_parts()
+    
     
     
--- analysis/timeblock.py
+++ analysis/timeblock.py
@@ -1,14 +1,32 @@
 from base.parts_model import PartsModel
 import dill
 import time
+import pandas as pd
 
 class TimeBlockModel(PartsModel):
     def __init__(self):
         super().__init__()
 
     def run_parts(self):
-        print("agent : ", self.agent)
-        print("ENV : ", self.environment)
+        #block_size = self.environment["parameter"]["timeblock"]  # 10초 기준 시간 블록 크기
+        block_size = 600
+        check_row = None
+        self.environment["df"]['timeblock'] = None
+        for index, row in self.environment["df"].iterrows():
+            if index < 1 :
+                self.environment["df"].loc[index, 'timeblock'] = 0
+                row['timeblock'] = 0
+                check_row = row
+                continue
+            
+            if (row['time'] - check_row['time']).total_seconds() < block_size :
+                self.environment["df"].loc[index,'timeblock'] = check_row['timeblock']
+                row['timeblock'] = check_row['timeblock']
+            else :
+                self.environment["df"].loc[index, 'timeblock'] = check_row['timeblock'] + 1
+                row['timeblock'] = check_row['timeblock'] + 1
+            check_row = row
+        print(self.environment["df"])
 
 
 if __name__ == "__main__":
